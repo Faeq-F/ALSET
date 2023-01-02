091eaff23a649ce93debc3c32db7ced1303488a4
Faeq <zlac318@live.rhul.ac.uk>
2023-01-02 14:22:16 +0000

    First app build with OpenCV integration

---
+++ app/src/main/java/com/faeq/self_drivingcar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.faeq.self_drivingcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()) Log.d("LOADED", "OpenCV success");
+        else Log.d("LOADED", "OpenCV error");
+    }
+}
---
+++ openCV/build/intermediates/cxx/Debug/6s6i6w51/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Faeq\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Faeq\\AndroidStudioProjects\\SelfDrivingCar\\openCV\\.cxx\\Debug\\6s6i6w51\\arm64-v8a" ^
+  opencv_jni_shared
