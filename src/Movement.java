e0cdbe816a6f8f1f7c0238c1473bf3246cd2a972
Faeq <Faeq@10.2.154.207>
2023-01-20 13:18:33 +0000

    Added partial boilerplate methods

--- src/Movement.java
+++ src/Movement.java
@@ -1,13 +1,27 @@
 
 
 public class Movement {
-	//add fields here - NO MAGIC NUMBERS
 	
-	//write methods here
+	private static int Delay_for_1DEG_Turn = 1;
+	private static int Delay_for_1CM_Forward = 1;
+	
+	public static void TurnLeft() {
+		System.out.println(Delay_for_1DEG_Turn);
+	}
+	
+	public static void TurnRight() {
+		System.out.println(Delay_for_1DEG_Turn);
+	}
+	
+	public static void MoveForward() {
+		System.out.println(Delay_for_1CM_Forward);
+	}
 
 	public static void main(String[] args) {
 		//test methods here
-
+		MoveForward();
+		TurnRight();
+		TurnLeft();
 	}
 
 }
