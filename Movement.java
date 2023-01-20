4ae5d4e8b25fe8e782ee40404152b315100d8aac
Faeq <Faeq@10.2.154.207>
2023-01-20 12:34:35 +0000

    Created Eclipse EV3 Project

---
+++ Movement.java
@@ -0,0 +1,9 @@
+
+public class Movement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Movement.java
@@ -0,0 +1,13 @@
+
+
+public class Movement {
+	//add fields here - NO MAGIC NUMBERS
+	
+	//write methods here
+
+	public static void main(String[] args) {
+		//test methods here
+
+	}
+
+}
