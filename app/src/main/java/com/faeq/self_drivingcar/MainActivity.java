4f9aa34a298f2bf8599e8cb9eabea7fa748fac1c
Faeq <zlac318@live.rhul.ac.uk>
2023-01-29 17:47:56 +0000

    First app build with working Color Detection

--- app/src/main/java/com/faeq/self_drivingcar/MainActivity.java
+++ app/src/main/java/com/faeq/self_drivingcar/MainActivity.java
@@ -6,9 +6,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 //Android imports
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 //OpenCV imports
@@ -16,9 +19,18 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+//Java imports; OpenCV methods require data to be given in lists of this type
+import java.util.List;
 //----------------------------------------------------------------------------------------------------------
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
     //------------------------------------------------------------------------------------------------------
     //Tag for activity (we only have one so this is the MainActivity)
     private static final String TAG = "MainActivity";
@@ -26,9 +38,18 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     JavaCameraView CameraView;
     //specifying that we are using the back camera (unable to specify wide-lens camera - maybe look into later)
     int activeCamera = CameraBridgeViewBase.CAMERA_ID_BACK;
-    Mat mRGBA;
     //Code for camera permissions
     private static final int MY_CAMERA_REQUEST_CODE = 100;
+    //Color detection
+    private boolean IsColorSelected = false;
+    private Mat mRgba;
+    private Scalar BlobColorRgba;
+    private Scalar BlobColorHsv;
+    private ColorBlobDetector Detector;
+    private Mat Spectrum;
+    private Size SpectrumSize;
+    private Scalar ContourColor;
+    private Scalar BoundingBoxColor;
     //------------------------------------------------------------------------------------------------------
     //initialises camera when app is first launched or when onResume is called from phone sleep
     private void initializeCamera(JavaCameraView CameraView, int activeCamera){
@@ -40,9 +61,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     //------------------------------------------------------------------------------------------------------
     //Enables Camera when OpenCV loads correctly (see onResume())
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
+        @SuppressLint("ClickableViewAccessibility")
         @Override
         public void onManagerConnected(int status) {
-            if (status == BaseLoaderCallback.SUCCESS) CameraView.enableView();
+            if (status == BaseLoaderCallback.SUCCESS){
+                CameraView.enableView();
+                CameraView.setOnTouchListener(MainActivity.this);
+            }
             else super.onManagerConnected(status);
         }
     };
@@ -58,6 +83,7 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         //show Main Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //show Camera View
         CameraView = findViewById(R.id.CameraView);
         //need permission checking above Android 5 (Our phone runs Android 9)
@@ -74,25 +100,100 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     //------------------------------------------------------------------------------------------------------
     //No need to manipulate frames - due to using back camera
     @Override
-    public void onCameraViewStarted(int width, int height) {}
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        Detector = new ColorBlobDetector();
+        Spectrum = new Mat();
+        BlobColorRgba = new Scalar(255);
+        BlobColorHsv = new Scalar(255);
+        SpectrumSize = new Size(200, 64);
+        ContourColor = new Scalar(0,255,0,255);
+        BoundingBoxColor = new Scalar(0,255,255,255);
+    }
     //------------------------------------------------------------------------------------------------------
-    @Override
-    public void onCameraViewStopped() { try {mRGBA.release();} catch(NullPointerException e){Toast.makeText(this, "Started getting frames", Toast.LENGTH_LONG).show();}}
-    //------------------------------------------------------------------------------------------------------
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mRGBA = inputFrame.rgba();
-        return mRGBA;
+    private Scalar convertScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
     }
     //------------------------------------------------------------------------------------------------------
     @Override
-    public void onPointerCaptureChanged(boolean hasCapture) { super.onPointerCaptureChanged(hasCapture); }
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+
+        if (IsColorSelected) {
+            Detector.process(mRgba);
+            List<MatOfPoint> contours = Detector.getContours();
+            //draw contours found
+            Imgproc.drawContours(mRgba, contours, -1, ContourColor, 5);
+            //Draw a bounding box around all contours
+            for (MatOfPoint c : contours){
+                // Make sure contour area is large enough
+                if (Imgproc.contourArea(c) > 10)
+                    //Drawing rectangle
+                    Imgproc.rectangle(mRgba,Imgproc.boundingRect(c), BoundingBoxColor, 5);
+            }
+            //Color being searched for
+            Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+            colorLabel.setTo(BlobColorRgba);
+        }
+
+        return mRgba;
+    }
     //------------------------------------------------------------------------------------------------------
+    @SuppressLint("ClickableViewAccessibility")
     @Override
-    protected void onDestroy() { super.onDestroy(); if (CameraView != null) CameraView.disableView(); }
-    //------------------------------------------------------------------------------------------------------
-    @Override
-    protected void onPause() { super.onPause(); if (CameraView != null) CameraView.disableView(); }
+    public boolean onTouch(View v, MotionEvent event) {
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (CameraView.getWidth() - cols) / 2;
+        int yOffset = (CameraView.getHeight() - rows) / 2;
+
+        int x = (int)event.getX() - xOffset;
+        int y = (int)event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        BlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < BlobColorHsv.val.length; i++)
+            BlobColorHsv.val[i] /= pointCount;
+
+        BlobColorRgba = convertScalarHsv2Rgba(BlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + BlobColorRgba.val[0] + ", " + BlobColorRgba.val[1] +
+                ", " + BlobColorRgba.val[2] + ", " + BlobColorRgba.val[3] + ")");
+
+        Detector.setHsvColor(BlobColorHsv);
+
+        Imgproc.resize(Detector.getSpectrum(), Spectrum, SpectrumSize);
+
+        IsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
     //------------------------------------------------------------------------------------------------------
     @Override
     protected void onResume(){
@@ -116,4 +217,16 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
             initializeCamera(CameraView, activeCamera);
         } else Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
     }
+    //------------------------------------------------------------------------------------------------------
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) { super.onPointerCaptureChanged(hasCapture); }
+    //------------------------------------------------------------------------------------------------------
+    @Override
+    protected void onDestroy() { super.onDestroy(); if (CameraView != null) CameraView.disableView(); }
+    //------------------------------------------------------------------------------------------------------
+    @Override
+    protected void onPause() { super.onPause(); if (CameraView != null) CameraView.disableView(); }
+    //------------------------------------------------------------------------------------------------------
+    @Override
+    public void onCameraViewStopped() { try {mRgba.release();} catch(NullPointerException e){Log.d(TAG, "No frame to release");}}
 }
