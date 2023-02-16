4b3c9db659ecd7210737b9ddc49a631b5d47fdbc
LeonardoLoureiro <Leonardo.K.Loureiro@outlook.com>
2023-02-16 15:43:06 +0000

    Added comments to highlight the general idea of how robot should behave.

--- src/DetectObstruction.java
+++ src/DetectObstruction.java
@@ -2,7 +2,30 @@
 public class DetectObstruction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Turn 90 degrees on the stop
+		
+		// Also turn distance detection so that it points
+		// towards wall.
+		
+		// Reset tacho count
+		
+		// while loop, keep going until does not detect wall beside it
+		
+		// keep going forwards for a little bit
+		
+		// assign tacho count to a var
+		
+		// turn -90, do NOT change distance sensor position
+		
+		// keep going until wall is not detected on the side
+		// as obstruction may be a box.
+		
+		// turn -90 again
+		
+		// go forward the amount of tacho count we assigned before
+		
+		// then rotate 90. In theory, we should be back where robot 
+		// was when it was behind the box/obstruction.
 
 	}
 
