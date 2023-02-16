cb8fc98146d9711c72cc9c58418eb2ccbf93f1b5
Faeq <faeqfaisal@hotmail.co.uk>
2023-02-16 14:31:45 +0000

    Removed old movement methods for new driver-like ones

--- src/Movement.java
+++ src/Movement.java
@@ -2,26 +2,5 @@
 
 public class Movement {
 	
-	private static int Delay_for_1DEG_Turn = 1;
-	private static int Delay_for_1CM_Forward = 1;
 	
-	public static void TurnLeft() {
-		System.out.println(Delay_for_1DEG_Turn);
-	}
-	
-	public static void TurnRight() {
-		System.out.println(Delay_for_1DEG_Turn);
-	}
-	
-	public static void MoveForward() {
-		System.out.println(Delay_for_1CM_Forward);
-	}
-
-	public static void main(String[] args) {
-		//test methods here
-		MoveForward();
-		TurnRight();
-		TurnLeft();
-	}
-
 }
