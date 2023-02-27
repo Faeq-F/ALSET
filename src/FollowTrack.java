56ae8335504e246093d76cb638ee376ef9b3003e
Morris <Morris.Sardo.2022@live.rhul.ac.uk>
2023-02-27 15:56:08 +0000

    Added condiction in action method Added statement in takeControl method done suppress method

--- src/FollowTrack.java
+++ src/FollowTrack.java
@@ -1,17 +1,19 @@
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class FollowTrack implements Behavior{
 	
 	//using these vars as the fields to be checked until bluetooth code has been written
 	Boolean BTconnection = true;
 	Boolean TrackFound = true;
-	
 	Boolean _suppressed = false;
 
 	@Override
 	public boolean takeControl() {
 		//check if there is a bluetooth connection and if a track is found (just use the fields)
 		//not used for testing
+		if(BTconnection == true && TrackFound == true)
+			return true;
 		return false;
 		
 	}
@@ -26,13 +28,29 @@ public class FollowTrack implements Behavior{
 		String messageR = "turn_right";
 		//check if suppressed
 		
+		if(messageC == "stay_in_the_center") {
+			movement.forward();
+			Delay.msDelay(1000);
+		}
+		else if(messageL == "turn_left") {
+			movement.turnLeft();
+			Delay.msDelay(1000);
+		}
+		else if(messageR == "turn_right") {
+			movement.turnRight();
+			Delay.msDelay(1000);
+		}
 		//if not suppressed, follow message instructions
+		else
+			suppress();
+		
 	}
 
 	@Override
 	public void suppress() {
 		//change suppress field
 		//not used for testing
+		_suppressed = true;
 	}
 	
 	public static void main(String[] args) {
