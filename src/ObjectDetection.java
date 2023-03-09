220f2205140e6c2f8094d3e0fbe23230c75737da
LeonardoLoureiro <Leonardo.K.Loureiro@outlook.com>
2023-03-09 15:19:38 +0000

    Completed this behaviour, to integrate with rest of code

--- src/ObjectDetection.java
+++ src/ObjectDetection.java
@@ -2,11 +2,14 @@ import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
 
-public class ObjectDetection extends Thread{
-	EV3UltrasonicSensor us_sensor = new EV3UltrasonicSensor(SensorPort.S2);
+public class ObjectDetection extends Thread {
+	EV3UltrasonicSensor us_sensor = new EV3UltrasonicSensor(SensorPort.S1);
 	SampleProvider sp = us_sensor.getDistanceMode();
 
 	ObjectDetection(){}
+	
+	boolean stop_thread = false ;
+	
 	
 	public void run(){
 		float[] sample = new float[2] ;
@@ -16,6 +19,7 @@ public class ObjectDetection extends Thread{
 			
 			movement.setDistanceFromObject(sample[0]);
 			
+			if (stop_thread) break ;
 		}
 	}
 }
--- src/ObjectTraversal.java
+++ src/ObjectTraversal.java
@@ -10,6 +10,7 @@ import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.MovePilot;
 import lejos.robotics.subsumption.Behavior;
+import lejos.utility.Delay;
 
 public class ObjectTraversal implements Behavior{
 
@@ -33,114 +34,158 @@ public class ObjectTraversal implements Behavior{
 	//	- To make robot rotate ~90 degrees on the spot, make it rotate by 200 amounts.
 	//	- The ultrasonic sensor motor's turn is clockwise relative to the top of the robot,
 	//	  meaning that 90 makes it turn to its right, 0 to the front and so on. 
-	//	- REMEMBER, ultrasonic's motor is set 0 as when PROGRAM STARTS, there is no absolute.
+	//	- REMEMBER, ultrasonic's motor is set 0 as when PROGRAM STARTS, there is no absolute.	
+	final static int SENSORMOTOR90TURN = 100 ;
 	
+	final static float WALLLENFROMROBOT = 0.126f ;
 	
-	final static float WHEEL_DIAMETER = 51; // The diameter (mm) of the wheels
-	final static float AXLE_LENGTH = 44; // The distance (mm) your two driven wheels
-	final static float ANGULAR_SPEED = 100; // How fast around corners (degrees/sec)
-	final static float LINEAR_SPEED = 70; // How fast in a straight line (mm/sec)
+	// about 235...
+	final static int ROT90DEGREES = 235 ;
 	
-	final static int SENSORMOTOR = 100 ;
-	final static int TRAVELAFTEROBS = 90 ;
-	final static int ROT90DEGREES = 200 ;
+	final static int DELAYBY = 1000 ;
 	
 	public static void main(String[] args) {
+		ObjectDetection OD = new ObjectDetection() ;
+		
+		OD.start();
+		
 		// ULTRASONIC SENSOR PARTS
 		// assign tacho to vars
 		int tach_start, tach_end ;
-		
-		ObjectDetection OD = new ObjectDetection();
-		OD.start();
-		
+				
 		// ULTRASONIC SENSOR MOTOR
 		BaseRegulatedMotor distanceSensorMotor = new EV3MediumRegulatedMotor(MotorPort.C) ;
 		// /ULTRASONIT SENSOR MOTOR
-		
 		
 		// WHEEL PARTS
 		BaseRegulatedMotor mLeft = new EV3LargeRegulatedMotor(MotorPort.A) ;
 		BaseRegulatedMotor mRight = new EV3LargeRegulatedMotor(MotorPort.D) ;
 		
 		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		// make a ~90 degree turn on the stop		
+		mLeft.rotate(ROT90DEGREES, true) ;
+		mRight.rotate(-ROT90DEGREES, true) ;
+		mLeft.endSynchronization() ;
 		
-		// Create a Wheel with Diameter 51mm and offset 22mm left of centre.
-		Wheel wLeft = WheeledChassis.modelWheel(mLeft, WHEEL_DIAMETER).offset(-AXLE_LENGTH / 2);
-				
-		// Create a Wheel with Diameter 51mm and offset 22mm right of centre.
-		Wheel wRight = WheeledChassis.modelWheel(mRight, WHEEL_DIAMETER).offset(AXLE_LENGTH / 2);
-		
-		// Create a Chassis with two wheels on it.
-		Chassis chassis = new WheeledChassis( (new Wheel[] {wRight, wLeft}),
-											   WheeledChassis.TYPE_DIFFERENTIAL);
-		
-		// Finally create a pilot which can drive using this chassis.
-		MovePilot pilot = new MovePilot(chassis);
-		
-		pilot.setAngularSpeed(ANGULAR_SPEED) ;
-		pilot.setLinearSpeed(LINEAR_SPEED) ;		
-		// /WHEEL PARTS
-	
-		
-		// Turn 90 degrees on the stop
-		pilot.rotate(ROT90DEGREES) ;
+		mLeft.waitComplete() ;
+		mRight.waitComplete() ;		
 		
 		// Also turn distance detection so that it points
 		// towards wall.
-		distanceSensorMotor.rotateTo(SENSORMOTOR) ;		
-		
+		distanceSensorMotor.rotateTo(SENSORMOTOR90TURN) ;		
 		
 		// 		KEEP GOING UNTIL WALL IS NO LONGER DETECTED
 		// Get starting tacho count, this will be used later
 		tach_start = mLeft.getTachoCount() ;
 		
+		// keep going until no more obstruction detected
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.forward() ;
+		mRight.forward() ;
+		mLeft.endSynchronization() ;
 		
-		
-		// while loop, keep going until does not detect wall beside it
-		pilot.forward() ;		
 		
 		float current_distance ;
 		while(true) {
 			current_distance = movement.getDistanceFromObject();
-			if (current_distance >= 100) {
+			if (current_distance >= WALLLENFROMROBOT) {
 				break;
-			}	
+			}
 		}
 		
-	
-		// keep going forwards for a little bit
-		pilot.travel(TRAVELAFTEROBS) ;
+		// keep going forwards for a little bit then stop
+		Delay.msDelay(DELAYBY) ;
 		
 		// assign tacho count to a var
-		tach_end = mLeft.getTachoCount() ;
+		tach_end = mLeft.getTachoCount() ;		
+		
+		// stop robot
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.stop(true) ;
+		mRight.stop(true) ;
+		mLeft.endSynchronization() ;
 		
 		
 		// turn -90, do NOT change distance sensor position
-		pilot.rotate(-ROT90DEGREES) ;
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.rotate(-ROT90DEGREES, true) ;
+		mRight.rotate(ROT90DEGREES, true) ;
 		
+		mLeft.waitComplete() ;
+		mRight.waitComplete() ;
+		
+		mLeft.forward() ;
+		mRight.forward() ;
+		
+		mLeft.endSynchronization() ;
+		
+				
+		// keep going forwards for a little bit then stop
+		Delay.msDelay(DELAYBY) ;
 		
 		// keep going until wall is not detected on the side
 		// as obstruction may be a box.
 		while(true) {
 			current_distance = movement.getDistanceFromObject();
-			if (current_distance >= 100) {
+			if (current_distance >= WALLLENFROMROBOT) {
 				break;
 			}	
 		}
 		
-		// turn -90 again
-		pilot.rotate(-ROT90DEGREES) ;
+		// keep going forwards for a little bit then stop
+		Delay.msDelay(DELAYBY) ;
+				
+		// stop robot
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.stop(true) ;
+		mRight.stop(true) ;
+		// mLeft.waitComplete() ;
+		// mRight.waitComplete() ;
+		mLeft.endSynchronization() ;
 		
+		
+		
+		// turn -90 again
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.rotate(-ROT90DEGREES, true) ;
+		mRight.rotate(ROT90DEGREES, true) ;
+		
+		mLeft.waitComplete() ;
+		mRight.waitComplete() ;
+		
+		mLeft.endSynchronization() ;
+		
+				
 		// go forward the amount of tacho count we assigned before
-		float distance_travelled = WHEEL_DIAMETER*3.14159f ;
-		pilot.travel(distance_travelled) ;
+		float distance_travelled = tach_end - tach_start ;
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.rotate((int) distance_travelled, true) ;
+		mRight.rotate((int) distance_travelled, true) ;
+		
+		mLeft.waitComplete() ;
+		mRight.waitComplete() ;
+		
+		mLeft.endSynchronization() ;
 		
 		// then rotate 90. In theory, we should be back where robot 
 		// was when it was behind the box/obstruction.
-		pilot.rotate(ROT90DEGREES) ;
-		distanceSensorMotor.rotateTo(SENSORMOTOR*0) ;	
+		mLeft.synchronizeWith(new BaseRegulatedMotor[] {mRight}) ;
+		mLeft.rotate(ROT90DEGREES, true) ;
+		mRight.rotate(-ROT90DEGREES, true) ;
 		
-		distanceSensorMotor.close() ;
+		mLeft.waitComplete() ;
+		mRight.waitComplete() ;
+		
+		mLeft.endSynchronization() ;
+		
+		distanceSensorMotor.rotateTo(SENSORMOTOR90TURN*0) ;	
+		
+		mLeft.close() ;
+		mRight.close() ;
+		
+		distanceSensorMotor.close() ;		
+		
+		OD.stop_thread = true ; 
 
 	}
 
