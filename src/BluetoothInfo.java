9f703f51e44ded7c4fe929db55bfb88a33de42c0
Faeq <faeqfaisal@hotmail.co.uk>
2023-03-11 16:22:25 +0000

    implemented BT connection

--- src/BluetoothInfo.java
+++ src/BluetoothInfo.java
@@ -1,27 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-
-import lejos.remote.nxt.BTConnector;
-import lejos.remote.nxt.NXTConnection;
+import java.net.Socket;
 
 public class BluetoothInfo extends Thread{
 	
 	BluetoothInfo(){}
 	
 	public void run(){
-		BTConnector connector = new BTConnector();
-		NXTConnection conn = connector.waitForConnection(0, NXTConnection.RAW);
-        InputStream IS = conn.openInputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(IS), 1);
-		while (true){
-			//get information from phone
-			try {
-				movement.setMessageFromPhone(br.readLine());
-	        } catch (IOException e) {
-	            e.printStackTrace(System.out);
-	        }
+		try {
+			Socket clientSocket = new Socket("10.0.1.2", 1234);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			while (true){
+				System.out.println(in.readLine());
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
+	    
 	}
+	
+	public static void main(final String[] array) {
+        final BluetoothInfo bluetoothInfo = new BluetoothInfo();
+        new ExitThread().start();
+        bluetoothInfo.run();
+    }
 }
